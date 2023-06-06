fd342724231968f3c783ba149c4a3eeb513a381d
cuixuyang615 <cuixuyang615@163.com>
2023-06-06 17:36:21 +0100

    challenge

--- counter_tb.cpp
+++ counter_tb.cpp
@@ -30,30 +30,30 @@ int main(int argc, char **argv, char **env)
             top->eval();
         }
 
-        // // original code:
-        // top->rst = (i<2) | (i==15);
-        // top->en  = (i>4);
+        // original code:
+        top->rst = (i<2) | (i==15);
+        top->en  = (i>4);
 
-        // challenge code:
-        top->rst = (i<2);
+        // // challenge code:
+        // top->rst = (i<2);
 
-        if(top->count == 0x9)
-        {
-            if(j<2)
-            {
-                top->en = 0;
-                j++;
-            }
-            else
-            {
-                top->en = 1;
-            }
-        }
-        else
-        {
-            top->en = 1;
-            j=0;
-        }
+        // if(top->count == 0x9)
+        // {
+        //     if(j<2)
+        //     {
+        //         top->en = 0;
+        //         j++;
+        //     }
+        //     else
+        //     {
+        //         top->en = 1;
+        //     }
+        // }
+        // else
+        // {
+        //     top->en = 1;
+        //     j=0;
+        // }
 
         if(Verilated::gotFinish())
             exit(0);
--- task1/readme.md
+++ task1/readme.md
@@ -1,1 +1,6 @@
-Create your design in this folder.
+# task1 result
+## original code result
+the original code waveform is shown as in Fig.1.
+![The waveform of original code](./original_waveform.PNG "The waveform of original code")
+the rst signal has been reseted when counter reaches 0x09, and this is because of:
+`code` top->rst = (i<2) | (i==15);
